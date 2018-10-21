af478e2d598d5a5708e3e7c0ac3cce9e666efc97
Vinicius Ribeiro <vinicius.tr@gmail.com>
2018-10-21 06:56:01 -0300

    added wizard component to questions page

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
-(function() {
-
-}).call(this);
+$(document).ready(function() {
+    var rw = document.getElementById('rootwizard');
+    if (rw) {
+        $(rw).bootstrapWizard({onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
+            var $percent = ($current/$total) * 100;
+            $('#rootwizard .progress-bar').css({width:$percent+'%'});
+        }});
+    }
+});
